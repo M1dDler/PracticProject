665a27b7c8cb369f698891ad5bc77727503f05c2
M1dDler <m1ddler.ua@gmail.com>
2023-02-28 02:59:50 +0200

    change InfNotifications

--- start.py
+++ start.py
@@ -54,6 +54,8 @@ async def balance_calldata(query):
     if data[0] == "InfNotification":
         city = getCityById(data[2])    
         notifications = getNotifications(data[1], data[2])
+        if len(notifications) == 0:
+            return await bot.send_message(query.from_user.id, "ℹ️ Поки що немає жодних активних сповіщень по місту " + city['city_name'] + ".")
         text = "<b>Список активних сповіщень для м." + city['city_name'] + "⤵️\n\n</b>"+"\n".join(str('📌 Черга - №'+str(x['city_group'])) for x in notifications)
         return await bot.send_message(query.from_user.id, text, parse_mode='HTML')
         
